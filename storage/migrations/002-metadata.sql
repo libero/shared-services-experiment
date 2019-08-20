b5a7ed4b159057752b1f3b26ee962a2e50f6c94a
Peter East <petereast+gpg@pm.me>
2019-08-20 13:37:19 +0100

    Add some basic database migrations

---
+++ storage/migrations/002-metadata.sql
@@ -0,0 +1,16 @@
+
+CREATE TABLE IF NOT EXISTS metadata (
+  id uuid default uuid_generate_v4(),
+  updated TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+  size int not null,
+  internalLink text,
+  sharedLink text,
+  publicLink text,
+  tags jsonb,
+  mimeType text not null,
+  namespace text not null,
+
+  primary key(id)
+);
+
+INSERT INTO migrations (identifier) VALUES ('metadata-table');
