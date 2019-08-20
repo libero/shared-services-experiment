c364927550898a95c92f4057206e110da85fbf4a
David Moulton <d.moulton@elifesciences.org>
2019-08-20 15:26:23 +0100

    Start graphQL client

--- client/src/GraphQLFileUploader.php
+++ client/src/GraphQLFileUploader.php
@@ -2,17 +2,62 @@
 
 namespace Libero\SharedServicesExperiment\Client;
 
+use InvalidArgumentException;
+use Softonic\GraphQL\Client as GraphQLClient;
+use Softonic\GraphQL\ResponseBuilder;
 use GuzzleHttp\ClientInterface;
+use function GuzzleHttp\Psr7\stream_for;
 
 class GraphQLFileUploader implements FileUploader
 {
+    private $client;
+
     public function __construct(ClientInterface $client)
     {
-        $this->client = new Client
+        $this->client = new GraphQLClient($client, new ResponseBuilder());
     }
 
-    public function uploadFile(string $sourcePath, string $uploadPath): array
+    public function uploadFile(string $sourcePath, string $uploadPath): FileUploadRecord
     {
+      if (! file_exists($sourcePath)) {
+        throw new InvalidArgumentException('File not found: ' . $sourcePath);
+      }
+
+      $finfo         = finfo_open(FILEINFO_MIME_TYPE);
+      $mimeType      = finfo_file($finfo, $sourcePath);
+      $contentLength = filesize($sourcePath);
+      $body          = stream_for(fopen($sourcePath, 'r'));
+      list(, $path) = explode('/', $uploadPath, 2) + [1 => null];
+
+      $query = <<<'QUERY'
+mutation UploadFile(file: Upload, meta: FileMeta) {
+        uploadFile {
+          id,
+          updated,
+          size,
+          internalLink,
+          sharedLink,
+          publicLink,
+          tags,
+          mimeType,
+          namespace
+        }
+      } 
+QUERY;
+
+      $queryVariables = [
+        'file' => $body,
+        'meta' => [
+          'mimeType' => $mimeType,
+          'size' => $contentLength,
+          'namespace' => dirname($uploadPath),
+          'path' => $path
+        ],
+      ];
+
+      $response = $this->client->query($query, $queryVariables);
 
     }
+
+
 }
