9ff5ebbfb450167ba7946e9af75ed42824a53048
Cory <c.donkin@elifesciences.org>
2019-08-21 10:08:38 +0100

    get aws credentials from config object

--- storage/src/s3Repository.js
+++ storage/src/s3Repository.js
@@ -1,18 +1,28 @@
 const AWS = require('aws-sdk');
 
+// to be replaced by config service
 const CONFIG = {
   someNameSpace:{
+    AWS_CREDENTIALS: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || '',
+      region: process.env.AWS_REGION || ''
+    },
     S3_BUCKET: 'storage-service-demo',
     S3_ACL: 'public'
   }
 }
 
-const s3 = new AWS.S3({
-  apiVersion: '2006-03-01',
-  signatureVersion: 'v4',
-});
+const getS3Instance = (config = {}) => {
+  return new AWS.S3({
+    ...config,
+    apiVersion: '2006-03-01',
+    signatureVersion: 'v4',
+  });
+}
 
 const putFile = (fStream, fData) => {
+  const s3 = getS3Instance(CONFIG[fData.namespace].AWS_CREDENTIALS);
   return s3
     .putObject({
       Body: fStream,
@@ -26,10 +36,12 @@ const putFile = (fStream, fData) => {
 }
 
 const getFile = (namespace, id) => {
+  const s3 = getS3Instance(CONFIG[namespace].AWS_CREDENTIALS);
   return s3.getObject({ Bucket: CONFIG[namespace].S3_BUCKET, Key: id })
 }
 
 const getSharedLink = (namespace, id) => {
+  const s3 = getS3Instance(CONFIG[namespace].AWS_CREDENTIALS);
   return s3.getSignedUrl('getObject', {
     Bucket: CONFIG[namespace].S3_BUCKET,
     Key: id,
